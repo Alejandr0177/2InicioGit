1e69fb7552b403d8f1697a0e28794d16044cc7a7
unknown <Alejandro0177>
2023-02-26 23:33:49 -0600

    Terminamos

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'Pollito Pio',
+        path: './music/Song 1.mp3',
+        artist: 'Los Topollos',
+        cover: '../images/cover 1.png'
+    },
+    {
+        name: 'AMG',
+        path: '../music/Song 2.mp3',
+        artist: 'Los Topollos',
+        cover: '../images/cover 2.png'
+    },
+    {
+        name: 'PRC',
+        path: '../music/Song 3.mp3',
+        artist: 'Los Topollos',
+        cover: '../images/cover 3.png'
+    },
+    {
+        name: 'Di que Si',
+        path: '../music/Song 4.mp3',
+        artist: 'Los Topollos',
+        cover: '../images/cover 4.png'
+    },
+    {
+        name: 'Fin de Semana',
+        path: '../music/Song 5.mp3',
+        artist: 'Los Topollos',
+        cover: '../images/cover 5.png'
+    }
+]
